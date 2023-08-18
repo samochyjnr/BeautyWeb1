2fbd9984ecfdc76110828b45e84670f0fb07bedc
-add <temtopeayobami@gmail.com>
2023-08-18 13:01:45 +0100

    Working on cats model

--- beautyapp/views.py
+++ beautyapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def home(request):
     record = Product.objects.all()
     return render(request, 'apps/home.html', {'record':record})
 
 def index(request):
-    return render(request, 'apps/index.html', {})
+    cat = Category.objects.all()
+    return render(request, 'apps/index.html', {'cat':cat})
