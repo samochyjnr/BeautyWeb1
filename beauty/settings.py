6218e5620a0a207bc023ccb96147303cffa04fe4
-add <temtopeayobami@gmail.com>
2023-09-13 14:26:02 +0100

    Done

--- beauty/settings.py
+++ beauty/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['willow-mm95.onrender.com', '127.0.0.1']
 
