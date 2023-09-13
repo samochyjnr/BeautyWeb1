92c165fb799fbdfcaed7ce411a7c24c66d95be23
-add <temtopeayobami@gmail.com>
2023-09-13 13:27:58 +0100

    Done

--- beauty/settings.py
+++ beauty/settings.py
@@ -173,7 +173,7 @@ JAZZMIN_SETTINGS = {
 import dj_database_url
 
 DATABASES = {
-   'default':dj_database_url.config(default=os.environ.get('DATABASE_URL'),
+   'default':dj_database_url.config(default=os.environ.get('DATABASES_URL'),
                                      
          conn_max_age=600,
          conn_health_checks=True,                            
