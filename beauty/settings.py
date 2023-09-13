815e2b78f16eaaf906a6687ab8e586269d138708
-add <temtopeayobami@gmail.com>
2023-09-13 13:36:34 +0100

    Done

--- beauty/settings.py
+++ beauty/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7k_@ot$v4s@f7^p0y0)mi8q$9blu!v3yo(nn7#nl@vf+zuty2!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
