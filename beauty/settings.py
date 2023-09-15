2ccc4188f46093b87708c6c0a52475cace91df95
-add <temtopeayobami@gmail.com>
2023-09-15 13:54:45 +0100

    Done

--- beauty/settings.py
+++ beauty/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'beauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -107,7 +107,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
