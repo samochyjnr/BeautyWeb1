f083745ab56b646adbf7a37855bda936f40649e6
-add <udemeakpainyang@gmail.com>
2023-09-15 10:31:48 +0100

    Done

--- beauty/settings.py
+++ beauty/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['willow-mm95.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['beauty-jqq8.onrender.com', '127.0.0.1']
 
 
 from dotenv import load_dotenv
