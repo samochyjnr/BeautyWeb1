bf607cb43e6c9f990871bb91e9840a1a492a9095
-add <udemeakpainyang@gmail.com>
2023-09-15 09:46:09 +0100

    Done

--- beauty/settings.py
+++ beauty/settings.py
@@ -185,7 +185,7 @@ import dj_database_url
 DATABASES = {
     'default' : dj_database_url.config (default=os.environ.get('DATABASE_URL'),
     conn_max_age=600,
-    
+    conn_health_checks=True,
     )
 }
 
